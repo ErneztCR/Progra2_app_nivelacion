0c735d49e563a18731b7c4197695d0bced594c1a
ErneztCR <49740650+ErneztCR@users.noreply.github.com>
2023-05-20 14:34:43 -0600

    Funcional_con_requerimientos_minimos_ver_1.1.1

---
+++ src/calcu_para_progra2/Calcu_para_Progra2.java
@@ -0,0 +1,18 @@
+
+package calcu_para_progra2;
+
+/**
+ *
+ * @author ervargas
+ */
+public class Calcu_para_Progra2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Interface inicio = new Interface();
+         inicio.setVisible(true);
+    }
+    
+}
---
+++ src/calcu_para_progra2/Cls_Operaciones.java
@@ -0,0 +1,10 @@
+
+package calcu_para_progra2;
+
+/**
+ *
+ * @author ervargas
+ */
+public class Cls_Operaciones {
+    
+}
